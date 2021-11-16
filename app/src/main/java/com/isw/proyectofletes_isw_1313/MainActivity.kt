ec6481c25917d40c02a9673fd0792929c66b948e
dbellocr <dbello0808@gmail.com>
2021-11-15 21:55:58 -0600

    Initial commit

---
+++ app/src/main/java/com/isw/proyectofletes_isw_1313/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.isw.proyectofletes_isw_1313
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/isw/proyectofletes_isw_1313/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.isw.proyectofletes_isw_1313.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
